31886afc67cb23330bc09fde2c4504975725d3cb
Cristian Muscalu <muscaiu@yahoo.com>
2017-05-27 13:27:07 +0300

    simplest state and setState example

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -4,10 +4,22 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 
 export default class Layout extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            name: 'Will'
+        }
+    }
+
     render() {
+        setTimeout(() => {
+            this.setState({ name: 'Cris' })
+        },2000)
+
         return (
             <div>
                 <Header />
+                {this.state.name}
                 <Footer />
             </div>
         )
