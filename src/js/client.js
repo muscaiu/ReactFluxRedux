1217bd551aeda7e4ed93baefe410f282fba0a40f
Cristian Muscalu <muscaiu@yahoo.com>
2017-05-27 12:32:24 +0300

    the most basic webpack ract phase

---
+++ src/js/client.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends Component {
+    render() {
+        return (
+            <h1>it works!</h1>
+        )
+    }
+}
+
+
+const app = document.getElementById('app')
+
+ReactDOM.render(<Layout />, app)
