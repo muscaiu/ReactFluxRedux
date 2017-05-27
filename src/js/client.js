0038081799a9ac1447abe00bfcacd7086745a8bf
Cristian Muscalu <muscaiu@yahoo.com>
2017-05-27 12:44:40 +0300

    basic examples of how to render data

--- src/js/client.js
+++ src/js/client.js
@@ -2,9 +2,20 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 class Layout extends Component {
+
+    constructor(){
+        super()
+        this.name = 'Will'
+    }
+
+    getVal(val) {
+        return 'w00f' + val
+    }
+
     render() {
+        const name = 'Cris'
         return (
-            <h1>it works!</h1>
+            <h1>it's {this.getVal('son')} - {name} - {this.name} </h1>
         )
     }
 }
