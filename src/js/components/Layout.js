5fb41935a13a99906de08e34ce5f88850e339fbc
Cristian Muscalu <muscaiu@yahoo.com>
2017-05-27 13:07:18 +0300

    example rendering a list of components

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,9 +4,14 @@ import Header from './Header'
 
 export default class Layout extends React.Component {
     render() {
+        var list = [
+            <Header />,
+            <Header />,
+            <Header />
+        ]
         return (
             <div>
-                <Header />
+                {list}
             </div>
         )
     }
