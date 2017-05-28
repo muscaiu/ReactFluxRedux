65ff81c145e2862b8168d98419062ccf652ac74e
Cristian Muscalu <muscaiu@yahoo.com>
2017-05-28 13:38:21 +0300

    passing a function though props, binding to this, changing input onChange

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -4,12 +4,26 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 
 export default class Layout extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            title: 'Welcome'
+        }
+    }
+
+    changeTitle(title) {
+        this.setState({ title: title })
+    }
+
     render() {
-        const title = "Welcome"
         const name = "Cris"
         return (
             <div>
-                <Header title={title} name={name} />
+                {/*bind this, otherwise it will not use the values from the Layout component*/}
+                <Header
+                    changeTitle={this.changeTitle.bind(this)}
+                    title={this.state.title}
+                    name={name} />
                 <Footer />
             </div>
         )
--- src/js/pages/components/Header.js
+++ src/js/pages/components/Header.js
@@ -3,11 +3,16 @@ import React from 'react'
 import Title from './Header/Title'
 
 export default class Header extends React.Component {
+    handleChange(e) {
+        const title = e.target.value
+        this.props.changeTitle(title)
+    }
     render() {
         return (
             <div>
                 {/*props is comming from Layout*/}
                 <Title title={this.props.title} name={this.props.name} />
+                <input onChange={this.handleChange.bind(this)} />
             </div>
         )
     }
